c73862c061504d8f1399a7dce986774ee40a6f1e
Kiransai321 <94594083+Kiransai321@users.noreply.github.com>
2022-07-19 09:56:20 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,43 +1,12 @@
+provider "aws" {
+  region     = "us-east-2"
+}
 
-resource "aws_instance" "web" {
-  ami           = "ami-02d1e544b84bf7502"
-  instance_type = "t2.micro"
-  #key_name = "saiapra"
-  iam_instance_profile =  aws_iam_instance_profile.test_profile.name
-  #role_arn = aws_iam_role.test_role.arn
-
-  metadata_options {
-    instance_metadata_tags = "enabled"
-     http_endpoint = "enabled"
-    http_tokens   = "required"
-  }
-
+resource "aws_s3_bucket" "bb" {
+ # bucket = "conformance-pack-aws"
+  #aws_s3_bucket_acl = "public"
   tags = {
-    Name = "saikiran"
+    Name        = "My bucket"
+    Environment = "test"
   }
-}
-resource "aws_iam_instance_profile" "test_profile" {
-  name = "test_profile"
-  role = aws_iam_role.test_role.name
-}
-
-resource "aws_iam_role" "test_role" {
-  name = "test_role"
-  path = "/"
-
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-               "Service": "ec2.amazonaws.com"
-            },
-            "Effect": "Allow",
-            "Sid": ""
-        }
-    ]
-}
-EOF
 }
